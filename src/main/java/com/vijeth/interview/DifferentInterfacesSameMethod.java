3eae7e775b93badb2d328a2fe4c2276943a6b597
vijeth <vijeth.7mar@gmail.com>
2019-01-19 10:02:16 +0530

    Java 8 Default and Static methods example

--- src/main/java/com/vijeth/interview/DifferentInterfacesSameMethod.java
+++ src/main/java/com/vijeth/interview/DifferentInterfacesSameMethod.java
@@ -2,21 +2,57 @@ package com.vijeth.interview;
 
 interface A{
     public void sayHi();
+
+    default public void sayHello(){
+        System.out.println("A's default method 'sayHello'");
+    }
+
+    default public void sayA(){
+        System.out.println("A's specific default method 'sayA'");
+    }
+
+    public static void sayYes(){
+        System.out.println("A's static method 'sayYes'");
+    }
 }
 
 interface B{
     public void sayHi();
+
+    default public void sayHello(){
+        System.out.println("B's default method 'sayHello'");
+    }
+
+    public static void sayYes(){
+        System.out.println("B's static method 'sayYes'");
+    }
 }
 
 class MyClass implements A, B{
     @Override
     public void sayHi() {
-        System.out.println("Hi");
+        System.out.println("MyClass sayHi");
     }
+
+    @Override
+    public void sayHello() {
+        System.out.println("Since this default method is common in both the interfaces- A and B, you have to provide the implementation here.");
+    }
+
+    public static void sayYes(){
+        System.out.println("MyClass's static method 'sayYes'");
+    }
+
 }
 
 public class DifferentInterfacesSameMethod {
     public static void main(String[] args) {
-        new MyClass().sayHi();
+        MyClass myClass = new MyClass();
+        myClass.sayHi();
+        myClass.sayHello();
+        myClass.sayA();
+        A.sayYes();
+        B.sayYes();
+        MyClass.sayYes();
     }
 }
